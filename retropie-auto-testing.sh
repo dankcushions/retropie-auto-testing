87f7f90a26d07db70766c7d97cafa4743f454072
dankcushions <chris.bransden@gmail.com>
2017-11-13 21:39:54 +0000

    Main execution loop now works.

--- retropie-auto-testing.sh
+++ retropie-auto-testing.sh
@@ -9,12 +9,23 @@ do
     esac
 done
 
-# Make SYSTEM lowercase
-SYSTEM=${BASH_REMATCH[1],,}
+# SYSTEM must be supplied
+echo $SYSTEM
+
+# Make sure Emulationstation is exited
 
 # Initialise variables
 ROOT_PATH="/home/pi/RetroPie/roms"
 SYSTEM_PATH="$ROOT_PATH/$SYSTEM"
+CONFIG_PATH="/opt/retropie/config"
+RUNCOMMAND="/home/pi/RetroPie-Setup/scriptmodules/supplementary/runcommand/runcommand.sh"
+RETROARCH="/opt/retropie/emulators/retroarch/bin/retroarch"
+RETROARCH_CFG="$CONFIG_PATH/all/retroarch.cfg"
+SYSTEM_CFG="$CONFIG_PATH/$SYSTEM/retroarch.cfg"
+# Location of Core binary. Hardcoded as SNES for now. Eventually get in same way as runcommand. ALSO MAKE SURE IT'S LIBRETRO!
+CORE="/opt/retropie/libretrocores/lr-snes9x2010/snes9x2010_libretro.so"
+# Time to let game run
+LOOP_LENGTH=10
 
 # Get permitted file extensions
 # For now, hardcoded for SNES
@@ -28,20 +39,21 @@ SYSTEM_EXTS[0]="swc"
 SYSTEM_EXTS[0]="zip"
 
 # Add all games with valid extensions in directory to an array
-local ext
-local games
-for ext in "${SYSTEM_EXTS[@]}"; do
-	cd $SYSTEM_PATH
-	games=(*.$ext)
-    SYSTEM_GAMES=("${SYSTEM_GAMES[@]}" "${games[@]}")
+cd $SYSTEM_PATH
+for EXT in "${SYSTEM_EXTS[@]}"; do
+    EXT_GAMES=(*.$EXT)
+    SYSTEM_GAMES=("${SYSTEM_GAMES[@]}" "${EXT_GAMES[@]}")
 done
-SYSTEM_PATH="$ROOT_PATH/$SYSTEM"
 
-# print test
-echo ${SYSTEM_GAMES[@]}
-echo ${#SYSTEM_GAMES[@]}
-
-# Loop through array, launching each game in verbose mode
+# Loop through array, launching each game
+for GAME in "${SYSTEM_GAMES[@]}"; do
+    GAME_PATH="$SYSTEM_PATH/$GAME"
+    echo $GAME_PATH
+    #echo timeout $LOOP_LENGTH $RUNCOMMAND 0 _SYS_ $SYSTEM $GAME_PATH
+    # Launch game with timeout
+    timeout $LOOP_LENGTH $RETROARCH -L $CORE --verbose --config $SYSTEM_CFG "$GAME_PATH"
+    #timeout $LOOP_LENGTH $RUNCOMMAND 0 _SYS_ $SYSTEM "$GAME_PATH"
+done
 
 ## Terminate after 60 seconds
 
